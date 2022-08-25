38287d59a7a2b3a814846e6b5d5e120d26875217
Daniel Bidulock <daniel@capitolhill.ca>
2022-08-25 12:09:20 -0600

    Get first meaningful English to French functionality

--- src/en_to_fr.py
+++ src/en_to_fr.py
@@ -1,4 +1,26 @@
+import re
+
+from fairseq.models.transformer import TransformerModel
+en2fr = TransformerModel.from_pretrained(
+  './models/nm-translation/wmt14.en-fr.joined-dict.transformer/',
+  checkpoint_file='model.pt',
+  # What is this?
+  #data_name_or_path='data-bin/wmt14_en_fr_full',
+  bpe='subword_nmt',
+  bpe_codes='bpecodes'
+)
 
 
-def add(a, b):
-  return a+b 
+def translate(phrase):
+  """
+  Translate a phrase from English to French
+  """
+  out = en2fr.translate(phrase)
+
+  # The NLLB stuff puts unwantedspaces between punctuation and words
+  #
+  # From https://stackoverflow.com/a/35141903
+  fix_spaces = re.compile(r'\s*([?!.,]+(?:\s+[?!.,]+)*)\s*')
+  out = fix_spaces.sub(lambda x: "{} ".format(x.group(1).replace(" ", "")), out).strip()
+
+  return out
--- tests/test_en_to_fr.py
+++ tests/test_en_to_fr.py
@@ -1,24 +1,23 @@
-
 # From https://www.youtube.com/watch?v=6tNS--WetLI
 
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
 
-import unittest
+import pytest
 import en_to_fr
-#from .context import en_to_fr
 
-class TestCase(unittest.TestCase):
+def test_translate_single_word():
+  """
+  Simple sanity check to ensure I've figured out how to test with Python
+  """
+  assert en_to_fr.translate("Hello") == "Bonjour"
 
-  def test_add(self):
+def test_translate_sentence():
+  """
+  Translate an entire sentence and correct punctuation spacing
+  """
+  assert en_to_fr.translate("In the beginning God created the heaven and the earth.") == "Au commencement, Dieu créa le ciel et la terre."
 
-    self.assertEqual(en_to_fr.add(10, 5), 15)
-    self.assertEqual(en_to_fr.add(-1, 1), 0)
-    self.assertEqual(en_to_fr.add(-1, -1), -2)
-
-# This makes it so I don't have to pass the unittest module from the commandline
-#if __name__ == '__main__':
-#    unittest.main()
 
 
